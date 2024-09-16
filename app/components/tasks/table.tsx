7920ec8011eed32e9721ca410639da154831b285
samuel267 <samthewebdev@gmail.com>
2024-09-16 11:25:21 +0300

    Ui update on the task state

--- app/components/tasks/table.tsx
+++ app/components/tasks/table.tsx
@@ -27,7 +27,7 @@ export default function TaskTable() {
 
   if (loading)
     return (
-      <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-start mt-20 h-screen">
         <ArrowPathIcon className="animate-spin h-12 w-12 text-blue-500" />
         <p className="text-lg text-blue-500 mt-4">Loading tasks...</p>
       </div>
@@ -35,19 +35,9 @@ export default function TaskTable() {
 
   if (error)
     return (
-      <div className="flex flex-col items-center justify-center h-screen">
+      <div className="flex flex-col items-centerf justify-start mt-20 h-screen">
         <ExclamationTriangleIcon className="h-12 w-12 text-red-500" />
         <p className="text-lg text-red-500 mt-4">Error loading tasks</p>
-      </div>
-    );
-
-  if (list.length === 0)
-    return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <ExclamationTriangleIcon className="h-12 w-12 text-yellow-500" />
-        <p className="text-lg text-gray-800 mt-4">
-          No tasks available. Add a new task
-        </p>
       </div>
     );
 
@@ -59,31 +49,40 @@ export default function TaskTable() {
         <div className="inline-block min-w-full align-middle">
           <div className="rounded-lg bg-gray-50 p-2 md:pt-0">
             <div className="md:hidden">
-              {list?.map((task) => (
-                <div
-                  key={task.id}
-                  className="mb-2 w-full rounded-md bg-white p-4"
-                >
-                  <div className="flex items-center justify-between border-b pb-4">
-                    <div>
-                      <div className="mb-2 flex items-center">
-                        <p>{task.title}</p>
-                      </div>
-                      <p className="text-sm text-gray-500">
-                        {task.description}
-                      </p>
-                      <p className="text-sm text-gray-500">{task.dueDate}</p>
-                    </div>
-                    <TaskStatus status={task.status} />
-                  </div>
-                  <div className="flex w-full items-center justify-between pt-4">
-                    <div className="flex justify-end gap-2">
-                      <UpdateTask id={task.id} />
-                      <DeleteTask id={task.id} />
-                    </div>
-                  </div>
+              {list.length === 0 ? (
+                <div className="flex flex-col items-center justify-center h-screen">
+                  <ExclamationTriangleIcon className="h-12 w-12 text-yellow-500" />
+                  <p className="text-lg text-gray-800 mt-4">
+                    No tasks available. Add a new task
+                  </p>
                 </div>
-              ))}
+              ) : (
+                list?.map((task) => (
+                  <div
+                    key={task.id}
+                    className="mb-2 w-full rounded-md bg-white p-4"
+                  >
+                    <div className="flex items-center justify-between border-b pb-4">
+                      <div>
+                        <div className="mb-2 flex items-center">
+                          <p>{task.title}</p>
+                        </div>
+                        <p className="text-sm text-gray-500">
+                          {task.description}
+                        </p>
+                        <p className="text-sm text-gray-500">{task.dueDate}</p>
+                      </div>
+                      <TaskStatus status={task.status} />
+                    </div>
+                    <div className="flex w-full items-center justify-between pt-4">
+                      <div className="flex justify-end gap-2">
+                        <UpdateTask id={task.id} />
+                        <DeleteTask id={task.id} />
+                      </div>
+                    </div>
+                  </div>
+                ))
+              )}
             </div>
             <table className="hidden min-w-full text-gray-900 md:table">
               <thead className="rounded-lg text-left text-sm font-normal">
@@ -106,35 +105,44 @@ export default function TaskTable() {
                   </th>
                 </tr>
               </thead>
-              <tbody className="bg-white">
-                {list?.map((task) => (
-                  <tr
-                    key={task.id}
-                    className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
-                  >
-                    <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                      <div className="flex items-center gap-3">
-                        <p>{task.title}</p>
-                      </div>
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-3">
-                      {task.description}
-                    </td>
+              <tbody className="bg-white relative">
+                {list.length === 0 ? (
+                  <div className="flex flex-col absolute w-full items-center justify-start mt-20 h-screen">
+                    <ExclamationTriangleIcon className="h-12 w-12 text-yellow-500" />
+                    <p className="text-lg text-gray-800 mt-4">
+                      No tasks available. Add a new task
+                    </p>
+                  </div>
+                ) : (
+                  list?.map((task) => (
+                    <tr
+                      key={task.id}
+                      className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
+                    >
+                      <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                        <div className="flex items-center gap-3">
+                          <p>{task.title}</p>
+                        </div>
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-3">
+                        {task.description}
+                      </td>
 
-                    <td className="whitespace-nowrap px-3 py-3">
-                      <TaskStatus status={task.status} />
-                    </td>
-                    <td className="whitespace-nowrap px-3 py-3">
-                      {task.dueDate}
-                    </td>
-                    <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                      <div className="flex justify-end gap-3">
-                        <UpdateTask id={task.id} />
-                        <DeleteTask id={task.id} />
-                      </div>
-                    </td>
-                  </tr>
-                ))}
+                      <td className="whitespace-nowrap px-3 py-3">
+                        <TaskStatus status={task.status} />
+                      </td>
+                      <td className="whitespace-nowrap px-3 py-3">
+                        {task.dueDate}
+                      </td>
+                      <td className="whitespace-nowrap py-3 pl-6 pr-3">
+                        <div className="flex justify-end gap-3">
+                          <UpdateTask id={task.id} />
+                          <DeleteTask id={task.id} />
+                        </div>
+                      </td>
+                    </tr>
+                  ))
+                )}
               </tbody>
             </table>
           </div>
